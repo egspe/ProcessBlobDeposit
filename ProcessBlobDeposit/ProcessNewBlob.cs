9eeff5cb8c02d95a58825ea92a860e321dd70b92
DESKTOP-KJD3JEG\gerem <geremy@ergonomicware.com>
2023-04-17 14:55:54 -0700

    Initial commit

---
+++ ProcessBlobDeposit/ProcessNewBlob.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace ProcessBlobDeposit
+{
+    public class ProcessNewBlob
+    {
+        [FunctionName("ProcessNewBlob")]
+        public void Run([BlobTrigger("samples-workitems/{name}", Connection = "BlobContainerConnectionString")] Stream inboundBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {inboundBlob.Length} Bytes");
+        }
+    }
+}
